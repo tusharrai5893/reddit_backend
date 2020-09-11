f0d20945fe67fe37872ab0f84209c3208a12f9c4
tusharrai5893 <rai.tushar5893@gmail.com>
2020-09-12 02:53:36 +0530

    populated post nd subreddit and pom file

--- src/main/java/com/reddit/backend/models/Post.java
+++ src/main/java/com/reddit/backend/models/Post.java
@@ -1,6 +1,42 @@
 package com.reddit.backend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long post_id;
+    @NotBlank(message = "Post Can't be Empty, Please Fill")
+    private String post_name;
+    @Nullable
+    private String url;
+    @Lob
+    private String description;
+    private Integer vote_count =0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_Id" , referencedColumnName = "user_Id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subreddit_Id" , referencedColumnName = "subreddit_Id")
+    private Subreddit subreddit;
+
+    private Instant createdDate;
 
     
 }
--- src/main/java/com/reddit/backend/models/Subreddit.java
+++ src/main/java/com/reddit/backend/models/Subreddit.java
@@ -1,4 +1,25 @@
 package com.reddit.backend.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import java.util.List;
+
 public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long subreddit_Id;
+    @NotBlank(message = "Community Name Can't be Empty")
+    private String community_Name;
+    @NotBlank(message = "Subreddit Description is Required")
+    private String description;
+    private Instant created_Date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Post> posts;
+
+
 }
